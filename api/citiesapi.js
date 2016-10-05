e8a8efc111ace83615d7180a9a0d76c8f6be4178
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-10-05 12:02:03 +0100

    cities api done

---
+++ api/citiesapi.js
@@ -0,0 +1,24 @@
+var CityApi = function(app) {
+
+  var cities = require("../client/src/models/cities");
+  var City = require("../client/src/models/city");
+
+  app.get("/api/cities", function(req, res) {
+    res.json({data: cities});
+  });
+
+  app.get("/api/cities/:id", function(req, res) {
+    var foundCity = req.params.id;
+    res.json({data: cities[foundCity]});
+  });
+
+  app.delete("/api/cities/:id", function(req, res) {
+    cities.splice(req.params.id, 1);
+    res.json({data: cities});
+  });
+
+  //add puts and post if time;
+
+}
+
+module.exports = FilmApi;
