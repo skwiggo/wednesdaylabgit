e3d26429c894491dd497cf4829a1ac83b4b2e11c
Charley Lawton <user@CODECLAN055.local>
2016-10-05 11:56:04 +0100

    cities completed

--- client/src/models/cities.js
+++ client/src/models/cities.js
@@ -1,1 +1,28 @@
+var City = require('./city');
+
+var Cities = function(){
+
+  var city1 = new City({
+    name: "Barcelona",
+    country: "Spain",
+    population: "5,375,774"
+  });
+
+  var city2 = new City({
+    name: "Vancouver",
+    country: "Canada",
+    population: "2,313,328"
+  });
+
+  var city3 = new City({
+    name: "Gaborone",
+    country: "Botswana",
+    population: "421,907"
+  });
+
+  return [city1, city1, city3];
+
+}
+
+
 module.exports = Cities;
