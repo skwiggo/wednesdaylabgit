a75e78e11e9258dec596cb12d92a1912726c7215
Charley Lawton <user@CODECLAN055.local>
2016-10-05 13:34:43 +0100

    API working

--- api/citiesapi.js
+++ api/citiesapi.js
@@ -1,20 +1,21 @@
 var CitiesApi = function(app) {
 
-  var cities = require("../client/src/models/cities");  
+  var Cities = require("../client/src/models/cities");  
   var City = require("../client/src/models/city");
 
+  var result = new Cities();
   app.get("/api/cities", function(req, res) {
-    res.json({data: cities});
+    res.json({data: result});
   });
 
   app.get("/api/cities/:id", function(req, res) {
     var foundCity = req.params.id;
-    res.json({data: cities[foundCity]});
+    res.json({data: result[foundCity]});
   });
 
   app.delete("/api/cities/:id", function(req, res) {
-    cities.splice(req.params.id, 1);
-    res.json({data: cities});
+    result.splice(req.params.id, 1);
+    res.json({data: result});
   });
 
   //add puts and post if time;
